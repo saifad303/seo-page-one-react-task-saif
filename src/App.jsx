e847d0ea237f25220d5c1ee0f10e5b3b0fe2a097
Saif <saifad303@gmail.com>
2023-08-27 02:24:52 +0600

    changed parent width

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Underreview from "./components/taskTypes/Underreview";
 const App = () => {
   return (
     <div className="flex items-start justify-center mt-10 h-screen">
-      <div className="w-[1536px] bg-white p-5 h-[85%] rounded-lg">
+      <div className="w-[90%] bg-white p-5 h-[85%] rounded-lg">
         <div className="border-2 border-slate-400 h-full rounded-lg px-2">
           <div className="flex items-start gap-4  overflow-x-auto p-4 h-full scrollbar-thin scrollbar-thumb-blue-900 scrollbar-track-gray-300">
             <Incomplete></Incomplete>
